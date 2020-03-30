7bba6f0aee862411910e39e3c7b598a21dd403cb
Ryan Allen <rcallen89@gmail.com>
2020-03-29 18:27:00 -0600

    Setup spec for api call

---
+++ spec/character_search_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe "Character Search API", type: :feature do
+  it 'when a house is searched for it returns characters that were in the OotP' do
+    visit root_path
+
+    select "Gryffindor", from: "house"
+
+    click_on("Search For Members")
+
+    expect(current_path).to eq(search_path)
+
+    expect(page).to have_content()
+  end
+end
